ab3d04e62c22461982d407df8c615b86ea928c48
17wolfgwang <dkzks44@gmail.com>
2023-06-28 01:24:25 -0400

    Update: ver 1.0 responsive web

--- src/pages/ServiceReviewsPage/ServiceReviewsPage.jsx
+++ src/pages/ServiceReviewsPage/ServiceReviewsPage.jsx
@@ -20,6 +20,18 @@ const slide_img = [
     "https://swiperjs.com/demos/images/nature-8.jpg",
     "https://swiperjs.com/demos/images/nature-9.jpg",
 ];
+const slide_img2 = [
+    "/images/reviews/mended1.jpeg",
+    "https://swiperjs.com/demos/images/nature-1.jpg",
+    "https://swiperjs.com/demos/images/nature-2.jpg",
+    "https://swiperjs.com/demos/images/nature-3.jpg",
+    "https://swiperjs.com/demos/images/nature-4.jpg",
+    "https://swiperjs.com/demos/images/nature-5.jpg",
+    "https://swiperjs.com/demos/images/nature-6.jpg",
+    "https://swiperjs.com/demos/images/nature-7.jpg",
+    "https://swiperjs.com/demos/images/nature-8.jpg",
+    "https://swiperjs.com/demos/images/nature-9.jpg",
+];
 
 const ServiceReviewsPage = () => {
     return (
@@ -29,31 +41,25 @@ const ServiceReviewsPage = () => {
             </div>
             <div className='reviewsSection'>
                 <Swiper
-                    effect={"coverflow"}
+                    effect="cube"
                     grabCursor={true}
-                    // centeredSlides={true}
-                    // slidesPerView={"auto"}
-                    coverflowEffect={{
-                        rotate: 50,
-                        stretch: 0,
-                        depth: 100,
-                        modifier: 1,
-                        slideShadows: false,
-                    }}
-                    pagination={true}
                     className="mySwiper"
-                    breakpoints={{
-                        640: {
-                            width: 640,
-                            slidesPerView: 3,
-                        },
-                        768: {
-                            width: 768,
-                            slidesPerView: 6,
-                        },
-                    }}
                 >
                     {slide_img.map((img, i) => {
+                        return (
+                            <SwiperSlide key={i}>
+                                <img src={img} alt="reviews" />
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
+                <Swiper
+                    effect="coverflow"
+                    grabCursor={true}
+                    navigation={true}
+                    className="mySwiper2"
+                >
+                    {slide_img2.map((img, i) => {
                         return (
                             <SwiperSlide key={i}>
                                 <img src={img} alt="reviews" />
