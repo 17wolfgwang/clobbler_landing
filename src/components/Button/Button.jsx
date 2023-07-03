4b175d24cd1810d7ea9e7d1b51225d154ab0bf37
17wolfgwang <dkzks44@gmail.com>
2023-07-03 01:01:02 -0400

    Update: ga event update

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import './Button.scss';
 const Button = () => {
     return (
         <>
-            <button className='reserveBtn'><a className="btnLink" href="http://pf.kakao.com/_gpeGxj/chat" target='_blank'>서비스 예약하기</a></button>
+            <button className='reserveBtn'><a id="btnLinkId" className="btnLinkClass" href="http://pf.kakao.com/_gpeGxj/chat" target='_blank'>서비스 예약하기</a></button>
         </>
     )
 }
